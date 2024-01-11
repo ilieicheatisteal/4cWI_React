c69b5de67ea5ab9f6427bd692e2c2666fc49f61c
ilieicheatisteal <eyupy99@gmail.com>
2024-01-11 13:36:10 +0100

    zweite

--- src/components/PeopleContainer.js
+++ src/components/PeopleContainer.js
@@ -5,9 +5,6 @@ export default function PeopleContainer() {
     return (
         <div>
             <h1>Weihnachten Charaktere</h1>
-            <div>
-                <Card name="Weihnachtsmann" title="Hauptcharakter" image="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Jonathan_G_Meath_portrays_Santa_Claus.jpg/1200px-Jonathan_G_Meath_portrays_Santa_Claus.jpg"/>
-        </div>
         </div>
     )
 }
