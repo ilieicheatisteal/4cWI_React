f3dcf3643e6fb778c01d400b493fbbfc125ea4f3
ilieicheatisteal <eyupy99@gmail.com>
2024-03-14 09:37:19 +0100

    zweite seite bilder

--- src/components/MatchDay.js
+++ src/components/MatchDay.js
@@ -16,7 +16,7 @@ export default function MatchDay() {
         <div className='pl-20 pr-20'>
             {Match.map(Match => (
                 <div className="border-8 h-20 flex justify-around items-center w-full p-8 border-red-600 rounded-2xl mb-5 text-2xl font-bold" key={""}>
-                    <div> {Match.team1.teamName}</div>|<div>{Match.team2.teamName}</div>
+                     <div><img src={Match.team1.teamIconUrl} className="h-10 w-10"/>{Match.team1.matchDateTime} {Match.team1.teamName}</div>|<div><img src={Match.team2.teamIconUrl} className="h-10 w-10"/> {Match.team2.teamName}</div>
                 </div>
             ))}
         </div>
